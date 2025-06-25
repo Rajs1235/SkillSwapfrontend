6ce2881b5b3df2aed39e1ce13e03c29dee2095f2
Raj Srivastava <161757777+Rajs1235@users.noreply.github.com>
2025-06-25 12:22:12 +0000

    bc

--- my-project/src/components/SavedCourse.jsx
+++ my-project/src/components/SavedCourse.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import api from './api';
-
+import axios from 'axios';
 function SavedCourse() {
   const [courses, setCourses] = useState([]);
 
