fe5d8b8b8d0c4488a3f3b54d3bfc0543e375ec29
Raj Srivastava <161757777+Rajs1235@users.noreply.github.com>
2025-06-28 06:30:25 +0000

    ok

--- my-project/src/components/api.js
+++ my-project/src/components/api.js
@@ -10,6 +10,8 @@ const api = axios.create({
 // ✅ Add token to request headers
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
+  console.log("Token:", localStorage.getItem("token"));
+
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   } else {
